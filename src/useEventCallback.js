a9cacebfd9c8dc8827785306ebf37f8de7a7d504
yanguangjie <yanguangjie@bytedance.com>
2020-06-27 16:00:00 +0800

    chore: add useEventCallback comments

--- src/useEventCallback.js
+++ src/useEventCallback.js
@@ -1,5 +1,8 @@
 import { useEffect, useCallback, useRef } from 'react';
 
+// https://zh-hans.reactjs.org/docs/hooks-faq.html
+// https://zhuanlan.zhihu.com/p/98554943
+// #useEventCallback
 export default function useEventCallback(fn, dependencies = []) {
   const ref = useRef(() => {
     throw new Error('Cannot call an event handler while rendering.');
