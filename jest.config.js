ade2bce1bdd28a63a4556dad3e04824ede6e57e6
yanguangjie <yanguangjie@bytedance.com>
2020-07-03 16:54:25 +0800

    test: add start and loading tests

--- jest.config.js
+++ jest.config.js
@@ -129,7 +129,7 @@ module.exports = {
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
   setupFilesAfterEnv: [
-    "@testing-library/jest-dom/extend-expect"
+    "@testing-library/jest-dom/extend-expect",
   ],
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
--- src/__tests__/app.js
+++ src/__tests__/app.js
@@ -5,10 +5,46 @@
 import React from 'react';
 import App from '../App.xstate';
 import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
 
-test('App should render search `input` & `button`', () => {
-  const { getByText, getByPlaceholderText } = render(<App />);
+describe('App start state', () => {
+  test('App should render search `input` & `button`', () => {
+    const { getByText, getByPlaceholderText } = render(<App />);
 
-  expect(getByText(/search/i)).toBeInTheDocument();
-  expect(getByPlaceholderText(/Search words by ur tags/i)).toBeInTheDocument();
+    expect(getByText(/search/i)).toBeInTheDocument();
+    expect(getByPlaceholderText(/Search words by ur tags/i)).toBeInTheDocument();
+  });
+});
+
+describe('App loading state', () => {
+  let container;
+
+  async function typeChar() {
+    const { getByText, getByPlaceholderText } = container;
+    const button = getByText(/search/i);
+    const input = getByPlaceholderText(/Search words by ur tags/i);
+
+    await userEvent.type(input, 'a');
+
+    userEvent.click(button);
+
+    return { input, button };
+  }
+
+  beforeEach(() => {
+    container = render(<App />);
+  });
+
+  test('searching button should change to `Searching`', async () => {
+    const { button } = await typeChar();
+
+    expect(button).toHaveTextContent(/searching\.{3}/i);
+  });
+
+  test('ui should display loading tips', async () => {
+    await typeChar();
+    const { queryByText } = container;
+
+    expect(queryByText(/loading\.{3}/i)).toBeInTheDocument();
+  });
 });
